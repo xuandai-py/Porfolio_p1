f6298611c908e9ee4dfd6d227ce39c9c2c9cca09
xuandai-py <buinguyenxuandai@gmail.com>
2021-06-23 16:16:40 +0700

    first-upload

---
+++ portfolio-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e23c17a65013ca37cafd73275a695519'),
+    },
+  },
+});
---
+++ portfolio-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd62d1520-bbbe-44bb-b226-8e08c8a831bc'
+};
